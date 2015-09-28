d962a208b0ec7fc81b7cedcdd4045b2243eb0405
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-09-28 23:52:14 +0200

    Added vagrantfile

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,14 @@ package_url = node['chef-server']['url']
 package_name = ::File.basename(package_url)
 package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
 
+if File.exists?(package_local_path)
+  log 'message' do
+    message "File already exists: #{package_local_path}"
+    level :info
+  end
+  return
+end
+
 # package is remote, let's download it
 remote_file package_local_path do
   source package_url
