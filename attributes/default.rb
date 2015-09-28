64f343b9ec17c3df05624d969cf8dabf06355875
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-09-29 01:15:06 +0200

    Tests

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,7 @@
 default['chef-server']['url'] = \
   'https://web-dl.packagecloud.io/chef/stable/packages/ubuntu/trusty/' \
   'chef-server-core_12.2.0-1_amd64.deb'
+default['chef-server']['file_download_path'] = '/vagrant/downloads'
 
 default['chef-server']['admin'] = {
   'user-name' => 'myuser',
@@ -8,11 +9,11 @@ default['chef-server']['admin'] = {
   'last-name' => 'MyUser',
   'email' => 'david.saenz.tagarro@gmail.com',
   'password' => '123456',
-  'rsa_private_key' => '~/myuser.pem'
+  'rsa_private_key' => '/home/vagrant/myuser.pem'
 }
 
 default['chef-server']['organization'] = {
   'org-name' => 'mychef',
   'full-name' => 'MyChef Org',
-  'rsa_private_key' => '~/mychef-validator.pem'
+  'rsa_private_key' => '/home/vagrant/mychef-validator.pem'
 }
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,25 +4,35 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+admin = node['chef-server']['admin']
+org = node['chef-server']['organization']
+
 package_url = node['chef-server']['url']
 package_name = ::File.basename(package_url)
-package_local_path = "#{Chef::Config[:file_cache_path]}/#{package_name}"
+package_path = nil
+package_cache_path = File.join(Chef::Config[:file_cache_path], package_name)
+package_download_path = File.join(node['chef-server']['file_download_path'], package_name)
 
-if File.exists?(package_local_path)
+if File.exists?(package_cache_path)
   log 'message' do
-    message "File already exists: #{package_local_path}"
+    message "Package found (cache): #{package_cache_path}"
     level :info
   end
+  package_path = package_cache_path
+elsif File.exists? package_download_path
+  log 'message' do
+    message "Package found (download): #{package_download_path}"
+    level :info
+  end
+  package_path = package_download_path
 else
   # package is remote, let's download it
-  remote_file package_local_path do
+  remote_file package_cache_path do
     source package_url
   end
 end
 
-package package_name do
-  source package_local_path
-  provider Chef::Provider::Package::Dpkg
+dpkg_package package_path do
   notifies :run, 'execute[reconfigure-chef-server]', :immediately
   notifies :run, 'execute[user-create]', :immediately
   notifies :run, 'execute[org-create]', :immediately
@@ -30,17 +40,9 @@ end
 
 execute 'reconfigure-chef-server' do
   command 'chef-server-ctl reconfigure'
-  action :nothing
+  # action :nothing
 end
 
-execute 'org-create' do
-  command "chef-server-ctl org-create " \
-    "#{org['org-name']} " \
-    "#{org['full-name']} " \
-    "--association_user #{admin['user-name']} " \
-    "--filename #{org['rsa_private_key']}"
-  action :nothing
-end
 
 execute 'user-create' do
   command "chef-server-ctl user-create " \
@@ -50,5 +52,14 @@ execute 'user-create' do
     "#{admin['email']} " \
     "#{admin['password']} " \
     "--filename #{admin['rsa_private_key']}"
-  action :nothing
+  # action :nothing
+end
+
+execute 'org-create' do
+  command "chef-server-ctl org-create " \
+    "#{org['org-name']} " \
+    "#{org['full-name']} " \
+    "--association_user #{admin['user-name']} " \
+    "--filename #{org['rsa_private_key']}"
+  # action :nothing
 end
